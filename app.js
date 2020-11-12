0ddd4e962f0821d660f835e6d9e6de4fa3562975
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-12 23:13:42 +0100

    adding CORS, Methods, Headers

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const app = express();
 // app.use(bodyParser.urlencoded()); --> for data type: x-www-form-urlencoded
 app.use(bodyParser.json()); // for data type: application/json
 
+// middleware -- headers
+app.use((req, res, next) => {
+  // add new headers except json() function default headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // middleware -- routes
 
 // ** forward any incoming requests that starts with ( /feed ) to the ( feeRoutes ) to handle them
