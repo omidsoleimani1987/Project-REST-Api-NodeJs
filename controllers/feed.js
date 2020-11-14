f00ba1581a193a820e267c8231c184c629b3f6b8
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-14 17:42:15 +0100

    working on creaet post

--- controllers/feed.js
+++ controllers/feed.js
@@ -28,9 +28,13 @@ exports.createPost = (req, res, next) => {
   res.status(201).json({
     message: 'Post created successfully',
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title,
-      content
+      content,
+      creator: {
+        name: 'omid'
+      },
+      createdAt: new Date()
     }
   });
 };
