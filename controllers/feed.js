8de33964f589b9dbb3b01f671cbc7f21cb7d7f5c
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-15 10:54:42 +0100

    adding update single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -35,9 +35,6 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
-  // !
-  console.log(req.file);
-
   // check is the file is set
   if (!req.file) {
     const message = 'No image provided!';
@@ -97,6 +94,67 @@ exports.getSinglePost = (req, res, next) => {
       res.status(200).json({
         message: 'Post fetched',
         post
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+// update and edit a single post
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  // adding validator to incoming requests
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // adding express error handling
+    const message = 'validation failed. entered data is incorrect.';
+    const error = new Error(message);
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // to check if the image url is changed or request contains new file inside it
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path.replace('\\', '/');
+  }
+  // if image url is not set
+  if (!imageUrl) {
+    const message = 'No file picked!';
+    const error = new Error(message);
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      // if we did not find the post
+      if (!post) {
+        const message = 'Could not find post.';
+        const error = new Error(message);
+        error.statusCode = 404;
+        throw error;
+      }
+
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+
+      return post.save();
+    })
+    .then(result => {
+      // when the post updated
+      res.status(200).json({
+        message: 'Post updated!',
+        post: result
       });
     })
     .catch(err => {
--- routes/feed.js
+++ routes/feed.js
@@ -25,4 +25,14 @@ router.post(
 // fetching a single post
 router.get('/post/:postId', feedController.getSinglePost);
 
+// updating the posts + adding validation to new values
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
 module.exports = router;
